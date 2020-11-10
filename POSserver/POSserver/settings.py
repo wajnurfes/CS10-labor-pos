e61c5d6a8936d60af55fbff7150211be6572236e
Zach Campbell <beazlebubbles@gmail.com>
2020-11-09 19:13:12 -0700

    hard code allowed host

--- POSserver/POSserver/settings.py
+++ POSserver/POSserver/settings.py
@@ -45,9 +45,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")]
-)
+ALLOWED_HOSTS = "mynewpsql.herokuapp.com"
 
 
 CORS_ALLOWS_METHODS = ("DELETE", "GET", "OPTIONS", "POST")
