b873ec215fa4c436b1f6a97a9a8b623c2625c314
Zach Campbell <beazlebubbles@gmail.com>
2020-11-18 00:48:13 -0700

    update service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
@@ -10,6 +9,7 @@ import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { setContext } from "apollo-link-context";
 import { AUTH_TOKEN } from "./constants";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const httpLink = createHttpLink({
   uri: process.env.REACT_APP_ENDPOINT,
@@ -39,4 +39,5 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-registerServiceWorker();
+
+serviceWorkerRegistration.register();
