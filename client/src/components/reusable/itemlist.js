d1eb07cbca5c4b96c02545763ff3cf46bcbae32c
Zach Campbell <beazlebubbles@gmail.com>
2020-11-18 10:28:27 -0700

    test color fix

--- client/src/components/reusable/itemlist.js
+++ client/src/components/reusable/itemlist.js
@@ -115,7 +115,7 @@ class ItemList extends Component {
         if (i % 2) item_class = classes.list_item_light;
         list_items.push(
           <ListItem key={i} dense button className={item_class}>
-            <Link to={`${path}/${current_item.id}`}>
+            <Link color='textPrimary' to={`${path}/${current_item.id}`}>
               <ListItemText>{current_item[name_field]}</ListItemText>
             </Link>
             <ListItemSecondaryAction>
